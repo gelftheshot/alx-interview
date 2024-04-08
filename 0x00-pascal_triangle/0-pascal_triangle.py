183b1543d38e31397761d1faa67d665e03a7efb3
Leon Gebre <gelf.learing@gmail.com>
2024-04-08 10:37:21 +0300

    passcales trinagel

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+def pascal_triangle(n):
+    res = [[1]]
+    prev = [1]
+
+    for i in range(1, n):
+        current = [1]
+        for j in range(1, i + 1):
+            if j > len(prev) - 1:
+                up = 0
+            else:
+                up = prev[j]
+            if j - 1 < 0:
+                right = 0
+            else:
+                right = prev[j-1]
+            current.append(up + right)
+        prev = current
+        res.append(current)
+    return(res)
