09a26df49bf13294d3a64ce1d3e9f86312b9395d
Leon Gebre <gelf.learing@gmail.com>
2024-04-11 22:23:50 +0300

    dong the job

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """ this is a function that will print the pascals triangel"""
+
+
 def pascal_triangle(n):
-    """ definging function that will handel the printing of 
+    """ definging function that will handel the printing of
         pascales triange
     """
     res = [[1]]
     prev = [1]
 
+    if n == 0:
+        return []
     for i in range(1, n):
         current = [1]
         for j in range(1, i + 1):
