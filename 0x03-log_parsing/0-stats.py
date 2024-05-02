5a7819ef62f5f52fd48828f4a558b60640b9af40
Leon Gebre <gelf.learing@gmail.com>
2024-05-02 22:08:37 +0300

    fixing and separating code inside a fucntion

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,27 +3,39 @@
 import sys
 
 
-stat_dic = {
-    '200': 0, '301': 0, '400': 0, '401': 0,
-    '403': 0, '404': 0, '405': 0, '500': 0
-}
-total = 0
-i = 0
-try:
-    for line in sys.stdin:
-        line_arr = line.split()
-        total += int(line_arr[-1])
-        stat_dic[line_arr[-2]] += 1
-        i += 1
-        if i == 10:
-            print(f'File size: {total}')
-            for key, value in stat_dic.items():
-                if value != 0:
-                    print(f'{int(key)}: {value}')
-            i = 0
-except KeyboardInterrupt:
-    print(f'File size: {total}')
-    for key, value in stat_dic.items():
-        if value:
-            print(key, ' : ', value)
-    raise
+if __name__ == "__main__":
+    stat_dic = {
+        '200': 0, '301': 0, '400': 0, '401': 0,
+        '403': 0, '404': 0, '405': 0, '500': 0
+    }
+    count = 1
+    file_size = 0
+
+    def Read_line(line):
+        """ Read, parse and grab data"""
+        try:
+            parsed_line = line.split()
+            status_code = parsed_line[-2]
+            if status_code in stat_dic.keys():
+                stat_dic[status_code] += 1
+            return int(parsed_line[-1])
+        except Exception:
+            return 0
+
+    def print_stats():
+        """print stats in ascending order"""
+        print("File size: {}".format(file_size))
+        for key in sorted(stat_dic.keys()):
+            if stat_dic[key]:
+                print("{}: {}".format(key, stat_dic[key]))
+
+    try:
+        for line in sys.stdin:
+            file_size += Read_line(line)
+            if count % 10 == 0:
+                print_stats()
+            count += 1
+    except KeyboardInterrupt:
+        print_stats()
+        raise
+    print_stats()
