f19899593d5a019bb6c52fc069d4ee85ee319168
Leon Gebre <gelf.learing@gmail.com>
2024-05-02 11:56:44 +0300

    slight change

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,16 +2,15 @@
 """ a python script to handle log """
 import sys
 
-total = 0
+
 stat_dic = {
     '200': 0, '301': 0, '400': 0, '401': 0,
     '403': 0, '404': 0, '405': 0, '500': 0
 }
-
+total = 0
 i = 0
 try:
     for line in sys.stdin:
-        line = line.strip()
         line_arr = line.split()
         total += int(line_arr[-1])
         stat_dic[line_arr[-2]] += 1
@@ -25,6 +24,6 @@ try:
 except KeyboardInterrupt:
     print(f'File size: {total}')
     for key, value in stat_dic.items():
-        if value != 0:
+        if value:
             print(key, ' : ', value)
     raise
