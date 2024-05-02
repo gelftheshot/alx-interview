52366c282dbfe7e72753a46be4abe4a430b8440e
Leon Gebre <gelf.learing@gmail.com>
2024-05-02 11:20:42 +0300

    pycodestyle and other stffs

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,28 +3,33 @@
 import sys
 
 total = 0
-stat_dic = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+stat_dic = {
+    '200': 0, '301': 0, '400': 0, '401': 0,
+    '403': 0, '404': 0, '405': 0, '500': 0
+}
 
 i = 0
 try:
-  for line in sys.stdin:
-    line = line.strip()
-    line_arr = line.split()
-    total += int(line_arr[-1])
-    stat_dic[line_arr[-2]] += 1
-    i += 1
-    if i == 10:
-      print(f'File size: {total}')
-      for key, value in stat_dic.items():
-        if value != 0:
-          sol = key + ': ' + str(value)
-          print(sol)
-      total = 0
-      i = 0
-      stat_dic = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
+    for line in sys.stdin:
+        line = line.strip()
+        line_arr = line.split()
+        total += int(line_arr[-1])
+        stat_dic[line_arr[-2]] += 1
+        i += 1
+        if i == 10:
+            print(f'File size: {total}')
+            for key, value in stat_dic.items():
+                if value != 0:
+                    print(key, ' : ', value)
+            total = 0
+            i = 0
+            stat_dic = {
+                '200': 0, '301': 0, '400': 0, '401': 0,
+                '403': 0, '404': 0, '405': 0, '500': 0
+            }
 except KeyboardInterrupt:
-  print(f'File size: {total}')
-  for key, value in stat_dic.items():
-    if value != 0:
-      print(key, ' : ', value)
-  raise
+    print(f'File size: {total}')
+    for key, value in stat_dic.items():
+        if value != 0:
+            print(key, ' : ', value)
+    raise
