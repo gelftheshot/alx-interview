100c956d98c3e90e37eaae9d1fe5e37e5dbb31a4
Leon Gebre <gelf.learing@gmail.com>
2024-05-23 22:13:46 +0300

    semi standard fixing

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,18 +6,26 @@ const movieId = process.argv[2] + '/';
 const movieApiUrl = 'https://swapi-api.hbtn.io/api/films/';
 
 httpRequest(movieApiUrl + movieId, async function (error, response, responseBody) {
-  if (error) return console.error(error);
+  if (error) {
+    console.error(error);
+    return;
+  }
 
   const characterUrlList = JSON.parse(responseBody).characters;
 
   for (const characterUrl of characterUrlList) {
     await new Promise(function (resolve, reject) {
       httpRequest(characterUrl, function (error, response, responseBody) {
-        if (error) return console.error(error);
+        if (error) {
+          console.error(error);
+          return reject(error);
+        }
 
         console.log(JSON.parse(responseBody).name);
         resolve();
       });
+    }).catch((error) => {
+      console.error(error);
     });
   }
 });
