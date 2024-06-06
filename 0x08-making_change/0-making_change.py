959c4443fd6f63c416a030e5203d4d04dc208a5c
Leon Gebre <gelf.learing@gmail.com>
2024-06-06 22:09:08 +0300

    minmum fucntion wrttten

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -3,8 +3,16 @@
     Given a pile of coins of different values, determine the
     fewest number of coins needed to meet a given amount total.
 """
-
 def makeChange(coins, total):
-    """
-        fewest number of coins needed to meet a given amount total
-    """
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0  # Base case: 0 coins are needed to make the total of 0
+
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if coin <= amount:
+                dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    return dp[total] if dp[total] != float('inf') else -1
+
