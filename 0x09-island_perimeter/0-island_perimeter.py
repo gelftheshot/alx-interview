54418039208b611a877e71b7ba11b8b5afb0e6c9
Leon Gebre <gelf.learing@gmail.com>
2024-06-22 10:48:33 +0300

    the module should be documented

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+    returns the perimeter of the island described in grid:
+    grid is a list of list of integers:
+        0 represents water
+        1 represents land
+        Each cell is square, with a side length of 1
+        Cells are connected horizontally/vertically (not diagonally).
+"""
 
 def island_perimeter(grid):
     """
